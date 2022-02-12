96edd450607b503b96bb62ab4faf84eabaeb3451
parkkong <eversumin@gmail.com>
2022-02-12 13:10:19 +0900

    first commit

--- src/pages/dashboard/dashboard.ts
+++ src/pages/dashboard/dashboard.ts
@@ -1,4 +1,38 @@
 import {Options, Vue} from 'vue-class-component';
+import axios from 'axios';
 
 @Options({})
-export default class Dashboard extends Vue {}
+export default class Dashboard extends Vue {
+
+    getResult: any;
+
+    data() {
+        return {
+            getResult : Array
+        }
+    }
+
+    getTicker() {
+        const crypto = "KRW-BTC,KRW-ETH,KRW-ORBS"; //비트코인 이더리움 오브스
+        const upbitUrl = "https://api.upbit.com/v1/ticker?markets=";
+
+        axios
+            .get(upbitUrl + crypto)
+            .then(res => {
+                this.getResult = res.data;
+            } 
+            )
+            .catch(err => {
+                console.log(err);
+            });
+    }
+  
+    created () {
+        this.getTicker;
+    }
+  
+    mounted () {
+        setInterval(this.getTicker,1000); //1초(1000) * 60
+    }
+    
+}
