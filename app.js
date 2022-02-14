bee3b3c2f15af84ca9840d4c168949b8629f730a
5mt2tm5 <5mt2tm5@gmail.com>
2022-02-14 17:35:41 +0900

    fourth commit

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
-const request = require('request');
 const cors = require('cors');
+const axiosApp = require('axios');
 
 const app = express();
 
@@ -9,12 +9,14 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true}));
 
 app.get('/api/tickers', function (req, res) {
-    request({
-        url: 'https://api.upbit.com/v1/ticker?markets=KRW-BTC',
-        method: 'GET'
-    }, (error, response, xml) => {
-        res.json(JSON.parse(response.body));
-    });
+
+    axiosApp
+            .get('https://api.upbit.com/v1/ticker?markets=KRW-BTC')
+            .then(response => {
+                res.json(response.data);
+            }).catch(async err => {
+                console.log(err);
+            })
 });
 
 app.listen(3000, () => console.log('Server Running at http://localhost:3000/api/tickers'));
